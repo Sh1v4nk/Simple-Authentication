827f583ca1520db5de597bdcfebd600b873ef5c0
Sh1v4nk <shivankpandey113@gmail.com>
2024-11-27 19:52:19 +0530

    fix(token): update SameSite attribute for authToken cookie to 'none' for cross-site usage

--- backend/src/utils/tokenUtils.ts
+++ backend/src/utils/tokenUtils.ts
@@ -24,7 +24,7 @@ export const generateTokenAndSetCookie = (
 
   res.cookie("authToken", token, {
     httpOnly: true,
-    sameSite: "strict",
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
     secure: process.env.NODE_ENV === "production",
   });
